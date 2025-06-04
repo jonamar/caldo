223b51b2bf499ffab78a6c2d090a111cc9d8388c
Jo <jon.amar@gmail.com>
2025-06-04 17:24:34 -0400

    style: new logo

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Calendar from "./components/Calendar";
 function App() {
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col items-center p-4">
-      <h1 className="text-2xl font-bold mb-4 text-center">My Tasks</h1>
+      <h1 className="text-2xl font-bold mb-4 text-center"><img src="/images/caldo-logo.svg" alt="Caldo Logo" /></h1>
       <Calendar />
     </div>
   );
